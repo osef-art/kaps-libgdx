b357203421b0a3000356e0d0c6f949db6216a783
adam <aadedjou@etud.u-pem.fr>
2021-01-24 22:17:39 +0100

    Added ideas

--- core/src/com/gdx/kaps/level/sidekick/SidekickRecord.java
+++ core/src/com/gdx/kaps/level/sidekick/SidekickRecord.java
@@ -23,6 +23,7 @@ public enum SidekickRecord {
     XERETH("Xereth", Color.COLOR_6, "slice", (lvl) -> lvl.applyToGrid(SidekickRecord::sliceRandomDiagonals), 25),
     JIM("Jim", Color.COLOR_10, "slice", (lvl) -> lvl.applyToGrid(SidekickRecord::sliceRandomLine), 25),
     COLOR("Color", Color.COLOR_11,"gen", SidekickRecord::generateSingleColoredGelule, -5),
+    // TODO: sidekick that burns 1 germ, 3dmg, 15 mana
     // TODO: sidekick that generates a single Caps ?
     ;
 
@@ -38,7 +39,7 @@ public enum SidekickRecord {
         this.name = name;
         this.type = type;
         // INFO: negative mana = cooldown
-        maxMana = mana > 0 ? 4 : 0; //TODO: <- testing purposes: Math.max(mana, 0);
+        maxMana = Math.max(mana, 0);
         cooldown = -Math.min(mana, 0);
         sound = soundName;
     }
@@ -74,6 +75,7 @@ public enum SidekickRecord {
     // powers
 
     private static void generateSingleColoredGelule(Level lvl) {
+        // TODO: make it the color of the sidekick/mate
         lvl.setNext(2, new Gelule(lvl, Color.random(lvl)));
     }
 
@@ -129,7 +131,7 @@ public enum SidekickRecord {
     private static void hitRandomTileAndAdjacents(Grid grid) {
         grid.pickRandomObject().ifPresent(
           obj -> {
-              grid.hit(obj.x(), obj.y(), FIRE);
+              grid.hit(obj.x(), obj.y(), FIRE, 2);
               grid.hit(obj.x() - 1, obj.y(), CORE);
               grid.hit(obj.x() + 1, obj.y(), CORE);
               grid.hit(obj.x(), obj.y() + 1, CORE);
